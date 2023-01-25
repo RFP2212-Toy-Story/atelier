7ac8d0244c567128690fe8fb5917f8bd34fe456c
August <AedonGrunGott@users.noreply.github.com>
2023-01-25 15:42:30 -0800

    first branch

---
+++ client/source/components/Overview.jsx
@@ -0,0 +1,18 @@
+
+// LIBRARY IMPORTS
+import React from 'react';
+
+
+// MAIN
+const App = () => {
+  console.info('OVERVIEW COMPONENT MOUNTED');
+
+  return (
+    <div>
+      <h1>BOW BEFORE YOUR NEW OVERLORD</h1>
+    </div>
+  );
+}
+
+
+export default App;
