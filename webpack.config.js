befda6d8ed10a5a0a582c552510228bc3b93db2e
Brett Austin Eastman <brett.austin.eastman@gmail.com>
2023-01-25 22:02:53 -0800

    Webpack/Babel setup

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: path.join(__dirname, 'client', 'source', 'index.jsx'),
+  output: {
+    path: path.join(__dirname, 'client', 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+
+
+};
+
+
+{
+
+}
