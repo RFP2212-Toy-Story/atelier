f963055a39678e8582b53b0e59e7dbf18c951b51
Brett Austin Eastman <brett.austin.eastman@gmail.com>
2023-01-27 10:00:56 -0800

    Add useContext functionality to App

--- client/source/components/questionsAnswers/QAModule.jsx
+++ client/source/components/questionsAnswers/QAModule.jsx
@@ -1,19 +1,19 @@
 import React, { useState, useContext } from 'react';
 import InputQuestion from './InputQuestion.jsx';
-import { ProdContext } from '../../ProdContext.js';
+// import { ProdContext } from '../../ProdContext.js';
 
 
 // MAIN
 const QAModule = () => {
 
   // const [questInput, setQuestInput] = useState('');
-  const {prodID, setProdID} = useContext(ProdContext);
+  // const {prodID, setProdID} = useContext(ProdContext);
 
   return (
     <div>
       <h3>Questions and Answers</h3>
       <InputQuestion />
-      <div>Product ID from QAModule: {prodID}</div>
+      {/* <div>Product ID from QAModule: {prodID}</div> */}
     </div>
   );
 }
