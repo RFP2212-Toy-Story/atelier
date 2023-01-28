c0c2bfc1fbcf49aa593bdc11a1817a6b574e79c4
August <AedonGrunGott@users.noreply.github.com>
2023-01-28 12:45:51 -0800

    collect and show code coverage

--- server/tests/basicTest.test.js
+++ server/tests/basicTest.test.js
@@ -1,6 +1,7 @@
 const add = require('./basicTest');
 
-test('ADD: expect 1 + 2 === 3', () => {
-  console.log(add(1, 2));
-  expect(add(1, 2)).toBe(3);
+describe('TESTING BASIC ADD', () => {
+  test('ADD: expect 1 + 2 === 3', () => {
+    expect(add(1, 2)).toBe(3);
+  });
 });
