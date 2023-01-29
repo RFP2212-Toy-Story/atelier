c2849430b784f016be2556649e5609c247d459a9
Aimee Kang <asrkang@gmail.com>
2023-01-29 14:57:42 -0800

    format date using date-fns

--- client/source/components/RR/ReviewTile.jsx
+++ client/source/components/RR/ReviewTile.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import { format, parseISO } from 'date-fns';
 
 const ReviewTile = function ReviewTile({ review }) {
+  const parsedDate = parseISO(review.date);
+  const formattedDate = format(parsedDate, 'PPP');
+
   return (
     <div className="review-tile">
+      <span className="review-header">
+        *****
+        {formattedDate}
+      </span>
       <div>{review.summary}</div>
     </div>
   );
