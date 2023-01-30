3a5beb32b51be52071ed405d11c8af6db750a283
Archaa Work <archaaworks@Archaas-MacBook-Air.local>
2023-01-30 14:11:28 -0800

    Dealing with ESLint stuff

--- client/source/AppHolder.jsx
+++ client/source/AppHolder.jsx
@@ -1,22 +1,20 @@
 // LIBRARY IMPORTS
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 // LOCAL IMPORTS
-import App from "./App.jsx";
+import App from './App.jsx';
 import { ProdContext } from './ProdContext.js';
 
-//MAIN
-const AppHolder = () => {
-
+// MAIN
+function AppHolder() {
   const [prodID, setProdID] = useState('product id');
-
   return (
     <div>
-      <ProdContext.Provider value={{prodID, setProdID}}>
-       <App />
+      <ProdContext.Provider value={{ prodID, setProdID }}>
+        <App />
       </ProdContext.Provider>
     </div>
-  )
+  );
 }
 
 export default AppHolder;
