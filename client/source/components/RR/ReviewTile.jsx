32658864239100c8938c1385bd020146660b6595
Aimee Kang <asrkang@gmail.com>
2023-01-29 16:45:45 -0800

    implement rating converter (numeric value to star value)

--- client/source/components/RR/ReviewTile.jsx
+++ client/source/components/RR/ReviewTile.jsx
@@ -5,10 +5,20 @@ const ReviewTile = function ReviewTile({ review }) {
   const parsedDate = parseISO(review.date);
   const formattedDate = format(parsedDate, 'PPP');
 
+  const starRating = {
+    1: '*',
+    2: '**',
+    3: '***',
+    4: '****',
+    5: '*****',
+  };
+
+  const convertRating = (rating) => starRating[rating];
+
   return (
     <div className="review-tile">
       <span className="review-header">
-        *****
+        {convertRating(review.rating)}
         {formattedDate}
       </span>
       <div>{review.summary}</div>
