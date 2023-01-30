d33a8a754b1b71522efdd3d9cc23a72f4250a573
August <AedonGrunGott@users.noreply.github.com>
2023-01-30 13:15:24 -0800

    convert eslintrc to JS style

---
+++ .eslintrc.js
@@ -0,0 +1,59 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended'
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  ignorePatterns: [
+    '*.map',
+    '*bundle.js*',
+    'node_modules'
+  ],
+  rules: {
+    'no-console': [
+      'error',
+      {
+        allow: [
+          'error',
+          'info',
+          'clear'
+        ]
+      }
+    ],
+    'no-multiple-empty-lines': [
+      'error',
+      {
+        max: 2
+      }
+    ],
+    'import/newline-after-import': [
+      'off'
+    ],
+    'react/jsx-one-expression-per-line': [
+      'off'
+    ],
+    'react/prop-types': [
+      'off'
+    ],
+    'comma-dangle': [
+      'error',
+      'never'
+    ],
+    'import/extensions': [
+      'off'
+    ]
+  }
+};
