a9ece6dde3c47552ebe578e21b12ef6803e76dac
Aimee Kang <asrkang@gmail.com>
2023-01-29 19:34:16 -0800

    render sort bar

--- client/source/components/RR/SortReviews.jsx
+++ client/source/components/RR/SortReviews.jsx
@@ -3,9 +3,17 @@ import React from 'react';
 const SortReviews = function SortReviews({ reviewCount, sortType, setSortType }) {
   return (
     <div className="sort-bar">
-      <span>
-        {reviewCount} reviews, sorted by
-      </span>
+      <label htmlFor={sortType}>
+        {reviewCount} reviews, sorted by { }
+        <select
+          value={sortType}
+          onChange={(event) => setSortType(event.target.value)}
+        >
+          <option value="relevance">relevance</option>
+          <option value="helpful">helpful</option>
+          <option value="newest">newest</option>
+        </select>
+      </label>
     </div>
   );
 };
