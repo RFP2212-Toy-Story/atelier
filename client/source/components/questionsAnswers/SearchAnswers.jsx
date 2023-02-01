850f323eac9a8db09cad061aa585547311ebb7c4
Brett Austin Eastman <brett.austin.eastman@gmail.com>
2023-02-01 11:32:43 -0800

    Add featureqa2 branch. Add input functionality to SearchAnswers

--- client/source/components/questionsAnswers/SearchAnswers.jsx
+++ client/source/components/questionsAnswers/SearchAnswers.jsx
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+// import qaData from './exampleDataQA';
 import { InputContainer } from './styles/Containers.styled';
 
 const SearchAnswers = function CreateSearchAnswersComponent() {
+  // const [answers, setAnswers] = useState(qaData.results[0].answers);
+  const [input, setInput] = useState('');
+  // const [query, setQuery] = useState('');
+  // console.log(query);
+
+  // const filtered = [...answers].filter(function(answer) {
+  //   return answer.body.toLowerCase().includes(input.toLowerCase())
+  // });
+
   return (
     <InputContainer>
       <input
         type="text"
         placeholder="Have a Question? Search For Answers"
+        onChange={(e) => setInput(e.target.value)}
+        value={input}
       />
-      <button type="button" onClick={() => {}}>Go!</button>
+      <button
+        type="button"
+        onClick={() => {
+          // setQuery(input);
+        }}
+      >Go!
+      </button>
     </InputContainer>
   );
 };
