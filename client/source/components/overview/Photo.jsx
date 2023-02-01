e993262dfceacfb9dce0642de87ac98b476a421b
August <AedonGrunGott@users.noreply.github.com>
2023-01-31 23:31:29 -0800

    fast average color for photo thumbnails

--- client/source/components/overview/Photo.jsx
+++ client/source/components/overview/Photo.jsx
@@ -1,11 +1,23 @@
 // LIBRARY IMPORTS
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { FastAverageColor } from 'fast-average-color';
 
 
 // MAIN
 const Photo = function CreatePhotoComponent({ photo }) {
+  const [averageColor, setAverageColor] = useState('#000000');
+  const fastAverageColor = new FastAverageColor();
+
+  console.info('Average Color', averageColor);
+
+  useEffect(() => {
+    fastAverageColor.getColorAsync(photo.thumbnail_url)
+      .then((color) => setAverageColor(color))
+      .catch((error) => { console.error(error); });
+  }, []);
+
   return (
-    <div className="photo">
+    <div className="photo" style={{ 'background-color': `${averageColor.hex}` }}>
       <img width="50%" alt="" src={photo.thumbnail_url} />
     </div>
   );
