095b3f5917f1bc423d42ec9f31e1bb924c40ff4e
Brett Austin Eastman <brett.austin.eastman@gmail.com>
2023-02-02 08:31:24 -0800

    Modal that could be used by other teammates

--- client/source/Modal.jsx
+++ client/source/Modal.jsx
@@ -7,26 +7,67 @@ const Wrapper = styled.div`
   background-color: white;
   border: 2px solid rgb(240, 240, 240);
   border-radius: 12px;
-  padding: 1rem;
-  position: absolute;
-  width: 600px;
+  position: fixed;
   top: 50%;
-  /* left: calc(50% - 125px); */
-  /* bottom: 100px;
-  right: 100px; */
   left: 50%;
-  margin: auto;
-  inset: 0;
-  /* height: 200px; */
+  transform: translate(-50%, -60%);
+  width: 600px;
+  max-width: 100%;
+  height: 500px;
+  max-height: 100%;
+  z-index: 1010;
+`;
+
+const ModalInner = styled.div`
+/* cover the modal */
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  box-sizing: border-box;
+  /* spacing as needed */
+  padding: 20px 50px 20px 20px;
+  /* let it scroll */
+  overflow: auto;
+`;
+
+const ModalOverlay = styled.div`
+  z-index: 1000;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.6);
+`;
+
+const CloseButton = styled.button`
+  position: absolute;
+  /* don't need to go crazy with z-index here, just sits over .modal-guts */
+  /* z-index: 1; */
+  cursor: pointer;
+  top: 10px;
+
+  /* needs to look OK with or without scrollbar */
+  right: 20px;
+
+  border: 0;
+  font-size: 1.3rem;
 `;
 
 export default function Modal({ children, isOpen, onClose }) {
   return (
     isOpen && createPortal(
-      <Wrapper>
-        <button type="button" onClick={onClose}>X</button>
-        {children}
-      </Wrapper>,
+      <>
+        <ModalOverlay />
+        <Wrapper>
+          <ModalInner>
+            <CloseButton type="button" onClick={onClose}>X</CloseButton>
+            {children}
+          </ModalInner>
+        </Wrapper>
+      </>,
       document.body
     )
   );
