c9458083e9c893a02270acd2bbef8866d2f34c17
Brett Austin Eastman <brett.austin.eastman@gmail.com>
2023-02-02 11:41:03 -0800

    Add API get request for questions

--- client/source/components/questionsAnswers/QAModule.jsx
+++ client/source/components/questionsAnswers/QAModule.jsx
@@ -1,14 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import SearchQuestions from './SearchQuestions';
 import LoadAddQA from './LoadAddQA';
 import DisplayPhotos from './DisplayPhotos';
 import QAList from './QAList';
+import * as requests from '../../utilities/axiosRequests.js';
 import qaData from './exampleDataQA';
 import { Container } from './styles/Containers.styled';
 
 const QAModule = function CreateQAModuleComponent() {
   const [questions, setQuestions] = useState(qaData.results);
+  // const [questionCount, setQuestionCount] = useState(qaData.results.length);
   const [query, setQuery] = useState('');
+
+  // for test only, to be deleted
+  const prodID = '40444';
+
+  const updateQuestions = () => {
+    requests
+      .get(`/qa/questions?product_id=${prodID}&count=4`)
+      .then((results) => {
+        setQuestions(results.data.results);
+        // setQuestionCount(results.data.results.length);
+      })
+      .catch((err) => console.error('Error with reviews request: ', err));
+  };
+
+  useEffect(() => updateQuestions(), []);
 
   const filteredQuestions = questions.filter(
     (question) => question.question_body.toLowerCase().includes(query.toLowerCase())
