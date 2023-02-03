a13a800f38a4a65009ce10ffc9ec17d759fb52df
Aimee Kang <asrkang@gmail.com>
2023-02-02 22:21:38 -0800

    move non-global helpers back to RatingBreakdown component, cleanup function name

--- client/source/utilities/helpers.js
+++ client/source/utilities/helpers.js
@@ -1,4 +1,4 @@
-const average = function calculateAverageRating(ratings) {
+const averageRating = function calculateAverageRating(ratings) {
   let count = null;
   let total = null;
   const breakdown = Object.entries(ratings);
@@ -9,30 +9,4 @@ const average = function calculateAverageRating(ratings) {
   return (total / count).toFixed(1);
 };
 
-const count = function calculateTotalRatings(ratings) {
-  return Object.keys(ratings).reduce((acc, key) => acc + Number(ratings[key]), 0);
-};
-
-const percentages = function createPercentageBreakdown(ratings) {
-  const result = {
-    1: 0,
-    2: 0,
-    3: 0,
-    4: 0,
-    5: 0
-  };
-  const total = count(ratings);
-  const breakdown = Object.entries(ratings);
-  breakdown.forEach((rating) => {
-    const ratingNum = Number(rating[0]);
-    const ratingCount = Number(rating[1]);
-    result[ratingNum] = Math.round((ratingCount / total) * 100);
-  });
-  return Object.entries(result);
-};
-
-export {
-  average,
-  count,
-  percentages
-};
+export default averageRating;
