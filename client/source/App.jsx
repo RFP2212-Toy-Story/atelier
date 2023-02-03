0b8b2c9c0db5b4391d6e03516aa739035b5c2a57
Archaa Work <archaaworks@Archaas-MacBook-Air.local>
2023-02-02 18:07:11 -0800

    Removed stars from app.jsx

--- client/source/App.jsx
+++ client/source/App.jsx
@@ -9,11 +9,6 @@ import OutfitList from './components/RelatedProducts/OutfitList';
 import QAModule from './components/questionsAnswers/QAModule';
 import ReviewModule from './components/RR/ReviewModule';
 import TestAPI from './utilities/TestAPI';
-import EmptyStar from './components/shared/EmptyStar.jsx';
-import QuarterStar from './components/shared/QuarterStar.jsx';
-import HalfStar from './components/shared/HalfStar.jsx';
-import ThreeQuarterStar from './components/shared/ThreeQuarterStar.jsx';
-import FullStar from './components/shared/FullStar.jsx';
 
 // MAIN
 const App = function CreateAppComponent() {
@@ -23,11 +18,6 @@ const App = function CreateAppComponent() {
     <div>
       <Header />
       <Overview />
-      <EmptyStar />
-      <QuarterStar />
-      <HalfStar />
-      <ThreeQuarterStar />
-      <FullStar />
       <RelatedList />
       <OutfitList />
       <QAModule />
