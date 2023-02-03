e4ca7a79955182ac290f0c261747c2478b11332a
Aimee Kang <asrkang@gmail.com>
2023-02-02 16:58:18 -0800

    save changes before switching branch

--- client/source/components/RR/RatingBreakdown.jsx
+++ client/source/components/RR/RatingBreakdown.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import calculateAverageRating from '../../utilities/helpers.js';
 
 const RatingBreakdown = function RatingsBreakdown({ meta }) {
+  // console.log('meta', meta);
+  // const { ratings } = meta.ratings;
+  // const averageRating = calculateAverageRating(meta.ratings);
+  // const totalRatings = Object.keys(ratings).reduce((acc, rating) => acc + ratings[rating], 0);
+
   return (
     <div className="rating-breakdown">
-      <h4>ratings breakdown placeholder</h4>
+      <div>{calculateAverageRating(meta.ratings)}</div>
     </div>
   );
 };
