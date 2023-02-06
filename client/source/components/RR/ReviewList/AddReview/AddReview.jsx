97ab9033a01ff0347866be5eb8d6bb92856d53f3
Aimee Kang <asrkang@gmail.com>
2023-02-05 16:34:46 -0800

    save latest form

--- client/source/components/RR/ReviewList/AddReview/AddReview.jsx
+++ client/source/components/RR/ReviewList/AddReview/AddReview.jsx
@@ -6,34 +6,70 @@ import traitOptions from '../../utilities/mappings.js';
 const AddReview = function AddReview() {
   const { product } = useContext(ProdContext);
   const traits = Object.entries(traitOptions);
-  // console.log('trait options', Object.entries(traitOptions));
 
   return (
     <form className="review-form">
       <div className="review-form-title">Write Your Review</div>
       <div className="review-form-subtitle">About the {product.name}</div>
-      <div className="overall-rating">placeholder</div>
       <div className="form-section">
-        <div className="form-section-header">Do you recommend this product?</div>
+        <div className="form-section-header">Overall rating*</div>
+        <div>placeholder for stars</div>
+      </div>
+      <div className="form-section">
+        <div className="form-section-header">Do you recommend this product?*</div>
         <div className="recommend-options">
           <label htmlFor="yes">
-            <input type="radio" id="recommend" value="yes" />
+            <input type="radio" id="option" value="yes" />
             Yes
           </label>
           <label htmlFor="no">
-            <input type="radio" id="recommend" value="no" />
+            <input type="radio" id="option" value="no" />
             No
           </label>
         </div>
       </div>
       <div className="form-section">
-        <div className="form-section-header"> Characteristics</div>
+        <div className="form-section-header"> Characteristics*</div>
+        <div className="required">(required)</div>
         {traits.map((trait) => (
           <Characteristic
             trait={trait}
             key={trait[0]}
           />
         ))}
+      </div>
+      <div className="form-section">
+        <div className="form-section-header">Review summary</div>
+        <input
+          type="text"
+          maxLength="60"
+          placeholder="Example: Best purchase ever!"
+        />
+      </div>
+      <div className="form-section">
+        <div className="form-section-header">Review body*</div>
+        <textarea
+          type="text"
+          minLength="50"
+          maxLength="1000"
+          placeholder="Why did you like the product or not?"
+        />
+        {/* <div className="body-counter"></> */}
+      </div>
+      <div className="form-section">
+        <div className="form-section-header">Upload your photos</div>
+      </div>
+      <div className="form-section">
+        <div className="form-section-header">What is your nickname*</div>
+        <input
+          type="text"
+          maxLength="60"
+          placeholder="Example: jackson11!"
+        />
+        <div className="form-section-footer">For privacy reasons, do not use your full name or email address</div>
+      </div>
+      <div className="form-section">
+        <div className="form-section-header">Your email*</div>
       </div>
     </form>
   );
--- client/source/components/RR/ReviewList/AddReview/Characteristic.jsx
+++ client/source/components/RR/ReviewList/AddReview/Characteristic.jsx
@@ -1,24 +1,21 @@
 import React from 'react';
 
 const Characteristic = function Characteristic({ trait }) {
-  const traitName = trait[0]; // 'Size'
-  const traitDescription = trait[1]; // [{label: too small, value: 1}]
+  const traitName = trait[0];
+  const traitDescription = trait[1];
 
   return (
     <div className="trait-options">
       <div className="trait-name">{traitName}</div>
       {traitDescription.map((option) => (
-        <div key={option.value}>
+        <label key={option.value} htmlFor={option.label}>
+          <input
+            type="radio"
+            id="option"
+            value={option.value}
+          />
           {option.label}
-          <label htmlFor={option.value}>
-            <input
-              type="radio"
-              id={traitName}
-              value={option.value}
-            />
-            {option.value}
-          </label>
-        </div>
+        </label>
       ))}
     </div>
   );
