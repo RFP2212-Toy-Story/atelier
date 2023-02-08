fe039f02a5ac4a358bc57f5d1f36620c937e2c06
August <AedonGrunGott@users.noreply.github.com>
2023-02-07 19:08:49 -0800

    the basic idea

--- client/tests/basicTest.jsx
+++ client/tests/basicTest.jsx
@@ -1,11 +1,30 @@
 /**
  * @jest-environment jsdom
  */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import * as requests from '../source/utilities/axiosRequests.js';
+
 
 const TestApp = function CreateTestApp() {
+  const [value, setValue] = useState('default value');
+
+  useEffect(() => {
+    requests.get('url_two')
+      .then((response) => { console.info('TWO GOT:', response.data); });
+
+    requests.get('url_one')
+      .then((response) => {
+        console.info('ONE GOT:', response.data);
+        setValue(response.data);
+      });
+  }, [value]);
+
   return (
-    <div>Hello World!</div>
+    <>
+      <div>Hello World!</div>
+      <div>{value}</div>
+    </>
   );
 };
 
--- client/tests/basicTest.test.jsx
+++ client/tests/basicTest.test.jsx
@@ -5,13 +5,33 @@ import React from 'react';
 import { render, screen } from '@testing-library/react'; // jest is a jerk about using ES6 import
 
 import TestApp from './basicTest';
+import * as requests from '../source/utilities/axiosRequests.js';
+
+jest.mock('../source/utilities/axiosRequests.js');
+requests.get.mockImplementation((url) => {
+  let data;
+
+  switch (url) {
+    case 'url_one':
+      data = 'data from ONE';
+      break;
+    case 'url_two':
+      data = 'data from TWO';
+      break;
+    default:
+      data = '404';
+  }
+
+  return (Promise.resolve({ data }));
+});
 
 describe('REACT: testing basic div rendering', () => {
   test('loads items eventually', async () => {
-    render(<TestApp />);
+    await render(<TestApp />);
 
     // Wait for page to update with query text
     const string = await screen.getByText('Hello World!');
+    screen.debug();
     expect(string.textContent).toBe('Hello World!');
   });
 });
