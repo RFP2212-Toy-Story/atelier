e53ada403333907ab2514e42ecd6316fa776a034
Archaa Work <archaaworks@Archaas-MacBook-Air.local>
2023-02-09 12:12:43 -0800

    Updates to OutfitList, OutfitListCard, and AddOutfitCard components

--- client/source/components/RelatedProducts/OutfitListCard.jsx
+++ client/source/components/RelatedProducts/OutfitListCard.jsx
@@ -1,21 +1,32 @@
 // LIBRARY IMPORTS
-import React from 'react';
+import React, { forwardRef } from 'react';
 import { IoIosCloseCircleOutline } from 'react-icons/io';
 
 // LOCAL IMPORTS
 import StyledOutfitListCard from './styles/OutfitListCard.styled.jsx';
 
 // MAIN
-const OutfitListCard = function CreateOutfitListCard() {
+// eslint-disable-next-line prefer-arrow-callback
+const OutfitListCard = forwardRef(function CreateOutfitListCard({ outfit, setOutfitItems }, ref) {
+  const outfitID = JSON.parse(outfit[0]);
+  const outfitData = JSON.parse(outfit[1]);
+
+  // HANDLERS
+  const handleRemoveClick = (e) => {
+    e.stopPropagation();
+    localStorage.removeItem(outfitID);
+    setOutfitItems({ ...localStorage });
+  };
+
   return (
-    <StyledOutfitListCard>
-      <img alt="imagePlaceholder" />
-      <IoIosCloseCircleOutline className="removeButton" />
-      <h3>CATEGORY</h3>
-      <h4>Name</h4>
-      <h5>$99.99</h5>
+    <StyledOutfitListCard ref={ref}>
+      <img alt="placeholder" src={outfitData.image} />
+      <IoIosCloseCircleOutline className="removeButton" onClick={handleRemoveClick} />
+      <h3>{outfitData.category}</h3>
+      <h4>{outfitData.name}</h4>
+      <h5>${outfitData.price}</h5>
     </StyledOutfitListCard>
   );
-};
+});
 
 export default OutfitListCard;
