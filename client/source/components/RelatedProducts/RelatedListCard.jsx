77e8ce6c8f1246211b1957dd0e3230ba0875514d
Archaa Work <archaaworks@Archaas-MacBook-Air.local>
2023-02-09 19:24:02 -0800

    Switching branches, no changes

--- client/source/components/RelatedProducts/RelatedListCard.jsx
+++ client/source/components/RelatedProducts/RelatedListCard.jsx
@@ -57,9 +57,9 @@ const RelatedListCard = forwardRef(function CreateRelatedListCard({ relatedProdI
       <StyledRelatedListCard onClick={handleCardClick} ref={ref}>
         <img alt={productDetail.name} src={imageURL} />
         <HiOutlineStar className="compareButton" onClick={onOpen} />
-        <h3 className="card-content" data-testid="card-category">{productDetail.category}</h3>
-        <h4 className="card-content">{productDetail.name}</h4>
-        <h5 className="card-content">${productDetail.default_price}</h5>
+        <h3 className="card-content" data-testid="card-category">{productDetail?.category}</h3>
+        <h4 className="card-content">{productDetail?.name}</h4>
+        <h5 className="card-content">${productDetail?.default_price}</h5>
         <AvgStarArray className="card-content" avgRating={avgRating} />
       </StyledRelatedListCard>
       <Modal isOpen={isOpen} onClose={onClose}>
